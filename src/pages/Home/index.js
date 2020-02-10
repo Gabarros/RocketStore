5ec81d48b89ba557f90e6654a66ef3f52cf281a5
Gabarros <gabarros17@gmail.com>
2020-02-10 11:49:58 -0300

    create product list styled component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+   <ProductList>
+     <li>
+       <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ims=120x" 
+       alt="Tenis-Nike" />
+       <strong>Tênis Nike Revolution 5</strong>
+       <span>R$ 129,90</span>
+
+       <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+       </button>
+     </li>
+     <li>
+       <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ims=120x" 
+       alt="Tenis-Nike" />
+       <strong>Tênis Nike Revolution 5</strong>
+       <span>R$ 129,90</span>
+
+       <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+       </button>
+     </li>
+     <li>
+       <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ims=120x" 
+       alt="Tenis-Nike" />
+       <strong>Tênis Nike Revolution 5</strong>
+       <span>R$ 129,90</span>
+
+       <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+       </button>
+     </li>
+     <li>
+       <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ims=120x" 
+       alt="Tenis-Nike" />
+       <strong>Tênis Nike Revolution 5</strong>
+       <span>R$ 129,90</span>
+
+       <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+       </button>
+     </li>
+     <li>
+       <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom1.jpg?ims=120x" 
+       alt="Tenis-Nike" />
+       <strong>Tênis Nike Revolution 5</strong>
+       <span>R$ 129,90</span>
+
+       <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+       </button>
+     </li>
+   </ProductList>
   )
 }
