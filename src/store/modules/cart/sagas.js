566933bada0f7261c0b83bd068e1ae8c9b14a23f
Gabarros <gabarros17@gmail.com>
2020-02-12 10:21:31 -0300

    Add stock verify on add product to cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,14 +1,42 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSucess } from './actions';
+import { addToCartSucess, updateAmount } from './actions';
 
-function* addToCart({ id }){
-  const response = yield call(api.get, `/products/${id}`);
+function* addToCart({ id }) {
+  const productExists = yield select(
+    state => state.cart.find(p => p.id === id),
+  );
 
-  yield put(addToCartSucess(response.data));
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if(amount > stockAmount){
+    console.tron.warn('erro');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmount(id, amount));
+  } else {
+
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price)
+    };
+
+    yield put(addToCartSucess(data));
+
+  }
 }
 
 export default all([
