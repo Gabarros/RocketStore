677e289e00b0963b42c0caf1edbb486bea054c8b
Gabarros <gabarros17@gmail.com>
2020-02-12 11:56:41 -0300

    Updates

---
+++ precache-manifest.28fcd1cbd77f4ae2ea00470b232fb682.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7522a586f0483bb3840451cd54c7ce38",
+    "url": "/RocketStore/index.html"
+  },
+  {
+    "revision": "00b341e75120e6bbab84",
+    "url": "/RocketStore/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "00b341e75120e6bbab84",
+    "url": "/RocketStore/static/js/2.75244ee1.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/RocketStore/static/js/2.75244ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9726c5bfebe689b9a7b6",
+    "url": "/RocketStore/static/js/main.74f9022e.chunk.js"
+  },
+  {
+    "revision": "769cc0a102c0e4412144",
+    "url": "/RocketStore/static/js/runtime-main.2baa3bcd.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/RocketStore/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/RocketStore/static/media/logo.879557c5.svg"
+  }
+]);
